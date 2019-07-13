3b004a820c907d20ffa2f8d196fea5747f54ae62
Bobbi Tarkany <bl.tarkany@gmail.com>
2019-07-13 09:37:12 -0700

    added textcontent to html. updated startgame function. Deleted photos and added photos. split title and grabbed number. updated pic title in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,24 @@
-// global variables
+// Global Variables
+// ======================================================================
 var userOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
 var movies = ["nightmare before christmas", "edward scissorhands", "sweeney todd", "beetlejuice", "corpse bride", "sleepy hollow", "frankenweenie"];
 var compGuess = "";
-var titleSplit = "";
 
 
 
 // word contanier
+var titleSplit = [];
+var numBlanks = 0;
+// n _ _ _ _
+var blanksAndLetters = [];      
+
 
 
 // game counters
 
 var winCount = 0;
 var lossCount = 0;
+var guessesLeft = 10;
 var lettersGuessed = [];
 
 
@@ -23,26 +29,48 @@ var gameMovie = document.getElementById("movieClass");
 var usedLetters = document.getElementById("letters");
 var winScore = document.getElementById("wins");
 var lossScore = document.getElementById("losses");
-var moviePic = document.getElementById("image")
+var moviePic = document.getElementById("image");
+var turnsLeft = document.getElementById("turns");
 
 
 
+// Global Functions
+// ===========================================================================
 
-// global functions
-
+// start of next round
 function startGame() {
 
+    titleSplit = compGuess.split("");
+    numBlanks = titleSplit.length;
+
+    // reset game counters
+    guessesLeft = 10;
+    lettersGuessed = [];
+
+    usedLetters.textContent = lettersGuessed;
+    turnsLeft.textContent = guessesLeft;
+    winScore.textContent = winCount;
+    lossScore.textContent = lossCount;
+    moviePic.setAttribute("src", "assets/images/blackandwhite.jpg");
+
+    // console log actions
+    console.log(titleSplit);
+    console.log(numBlanks);
 
 }
 
+// full restart of game
 function restartGame() {
 
 
 }
 
+// movie title selection 
 function movieTitle() {
 
     compGuess = movies[Math.floor(Math.random() * movies.length)];
+
+
 
     console.log(compGuess);
 }
@@ -54,6 +82,8 @@ function movieTitle() {
 
 
 
-// game logic
+// Game Logic
+// ============================================================================
 
 movieTitle();
+startGame();
